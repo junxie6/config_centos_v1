970b52e687185ce1e22ce7d0b4373da0aaaede0b
Jun Hsieh <jun.github@ijun.org>
2015-03-06 15:07:27 -0800

    up

--- script/setMagentoPermission.sh
+++ script/setMagentoPermission.sh
@@ -1,14 +1,40 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 magDir="/www/magento1.9.1_us"
 srcCtl=".svn .git"
 
 ### make sure apache is in dev group
 
+# isUserInGroup user group
+function isUserInGroup {
+  # \b is word boundary.
+  if id -Gn ${1} | grep &>/dev/null "\b${2}\b"; then
+    return 0 # true. exit status code
+  fi
+
+  return 1 # false exit status code
+}
+
+if ! isUserInGroup apache dev; then
+  usermod -a -G dev apache
+  systemctl restart httpd.service
+  systemctl restart php-fpm.service
+fi
+
 ### protecting the Magento directories and files
-find ${magDir}/ -not -path "*/\.svn*" -and -not -path "*/\.git*" | xargs -I {} chown dev:dev {}
-find ${magDir}/ -not -path "*/\.svn*" -and -not -path "*/\.git*" -type d | xargs -I {} chmod 750 {}
-find ${magDir}/ -not -path "*/\.svn*" -and -not -path "*/\.git*" -type f | xargs -I {} chmod 640 {}
+find ${magDir}/ -not -path "*/\.svn*" -and -not -path "*/\.git*" -print0 | xargs -0 -I {} chown dev:dev {}
+find ${magDir}/ -not -path "*/\.svn*" -and -not -path "*/\.git*" -type d -print0 | xargs -0 -I {} chmod 750 {}
+find ${magDir}/ -not -path "*/\.svn*" -and -not -path "*/\.git*" -type f -print0 | xargs -0 -I {} chmod 640 {}
+
+find ${magDir}/var/ -type d -print0 | xargs -0 -I {} chmod 770 {}
+find ${magDir}/var/ -type f -print0 | xargs -0 -I {} chmod 660 {}
+
+find ${magDir}/media/ -type d -print0 | xargs -0 -I {} chmod 770 {}
+find ${magDir}/media/ -type f -print0 | xargs -0 -I {} chmod 660 {}
+
+chcon -R -t httpd_sys_content_t ${magDir}/
+chcon -R -t httpd_sys_rw_content_t ${magDir}/var/
+chcon -R -t httpd_sys_rw_content_t ${magDir}/media/
 
 ### protecting the .htaccess file
 
@@ -17,12 +43,12 @@ for d in ${srcCtl}; do
   dd=${magDir}/${d}/
 
   if [[ -d ${dd} ]]; then
-    find ${dd} | xargs -I {} chown dev:dev {}
-    find ${dd} -type f | xargs -I {} chmod 600 {}
-    find ${dd} -type d | xargs -I {} chmod 700 {}
+    find ${dd} -print0 | xargs -0 -I {} chown dev:dev {}
+    find ${dd} -type f -print0 | xargs -0 -I {} chmod 600 {}
+    find ${dd} -type d -print0 | xargs -0 -I {} chmod 700 {}
   fi
 done
 
 ###
-chmod g+s ${magDir}/
+#chmod g+s ${magDir}/
 
