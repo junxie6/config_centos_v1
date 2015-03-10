4332f25a53feea4f76ec75c7fa1e9f4797468497
Jun Hsieh <jun.github@ijun.org>
2015-03-10 11:22:39 -0700

    up

--- script/setMagentoPermission.sh
+++ script/setMagentoPermission.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
 magDir="/www/magento1.9.1_us"
 srcCtl=".svn .git"
 
-### make sure apache is in dev group
+### check if it's running by root
 
-# isUserInGroup user group
+### make sure apache is in dev group
+### Usage: isUserInGroup user group
 function isUserInGroup {
   # \b is word boundary.
   if id -Gn ${1} | grep &>/dev/null "\b${2}\b"; then
@@ -36,7 +42,7 @@ chcon -R -t httpd_sys_content_t ${magDir}/
 chcon -R -t httpd_sys_rw_content_t ${magDir}/var/
 chcon -R -t httpd_sys_rw_content_t ${magDir}/media/
 
-### protecting the .htaccess file
+find ${magDir}/ -name '.htaccess' -type f -print0 | xargs -0 -I {} chcon -t httpd_sys_content_t {}
 
 ### protecting the .svn or .git directories
 for d in ${srcCtl}; do
@@ -46,9 +52,8 @@ for d in ${srcCtl}; do
     find ${dd} -print0 | xargs -0 -I {} chown dev:dev {}
     find ${dd} -type f -print0 | xargs -0 -I {} chmod 600 {}
     find ${dd} -type d -print0 | xargs -0 -I {} chmod 700 {}
+    
+    ### we should add the chon command here
   fi
 done
-
-###
-#chmod g+s ${magDir}/
 
