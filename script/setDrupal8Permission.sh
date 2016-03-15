8edf656f98d52d087b073c0712ce3b9e151be086
Jun Hsieh <jun.github@ijun.org>
2016-03-15 14:18:59 -0700

    up

--- script/setDrupal8Permission.sh
+++ script/setDrupal8Permission.sh
@@ -38,5 +38,35 @@ if ! isUserInGroup php-fpm web; then
   exit 1
 fi
 
+### basic setting for the directories and files.
+find ${magDir}/ -not -path "*/\.svn" -and -not -path "*/\.git" -print0 | xargs -0 -I {} chown dev:web {}
+find ${magDir}/ -not -path "*/\.svn" -and -not -path "*/\.git" -print0 | xargs -0 -I {} chcon -t httpd_sys_content_t {}
 
+find ${magDir}/ -not -path "*/\.svn" -and -not -path "*/\.git" -type d -print0 | xargs -0 -I {} chmod 2750 {}
+find ${magDir}/ -not -path "*/\.svn" -and -not -path "*/\.git" -type f -print0 | xargs -0 -I {} chmod 640 {}
+
+#chcon -R -t httpd_sys_rw_content_t ${magDir}/media/
+
+exit 0
+
+### for the .htaccess files
+find ${magDir}/ -name '.htaccess' -type f -print0 | xargs -0 -I {} chmod 640 {}
+find ${magDir}/ -name '.htaccess' -type f -print0 | xargs -0 -I {} chcon -t httpd_sys_content_t {}
+
+### for the .svn and .git directories.
+for d in ${srcCtl}; do
+  dd=${magDir}/${d}/
+
+  if [[ -d ${dd} ]]; then
+    find ${dd} -print0 | xargs -0 -I {} chown dev:dev {}
+    find ${dd} -type f -print0 | xargs -0 -I {} chmod 600 {}
+
+    find ${dd} -type d -print0 | xargs -0 -I {} chmod 0000 {}
+    find ${dd} -type d -print0 | xargs -0 -I {} chmod 700 {}
+    
+    ### we should add the chon command here
+    ### Disabled: because when I "svn update", the SELinux security context permission would become "user_home_t" instead of "httpd_sys_content_t".
+    #chcon -R -t user_home_t ${dd}
+  fi
+done
 
