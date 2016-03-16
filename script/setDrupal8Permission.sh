54251ec8383c708752e3565e64e79e711d1c4446
Jun Hsieh <jun.github@ijun.org>
2016-03-16 23:05:15 +0000

    up

--- script/setDrupal8Permission.sh
+++ script/setDrupal8Permission.sh
@@ -48,14 +48,12 @@ find ${druDir}/ -not -path "*/\.svn" -and -not -path "*/\.git" -type f -print0 |
 
 ### sites
 find ${druDir}/sites/ -mindepth 1 -maxdepth 1 -type d -not -path "sites/default" | while read line; do
-  dd=${druDir}/${line}
+  find ${line}/files/ -type d -print0 | xargs -0 -I {} chmod 2770 {}
+  find ${line}/files/ -type f -print0 | xargs -0 -I {} chmod 660 {}
+  find ${line}/files/ -print0 | xargs -0 -I {} chcon -t httpd_sys_rw_content_t {}
 
-  find ${dd}/files/ -type d -print0 | xargs -0 -I {} chmod 2770 {}
-  find ${dd}/files/ -type f -print0 | xargs -0 -I {} chmod 660 {}
-  find ${dd}/files/ -print0 | xargs -0 -I {} chcon -t httpd_sys_rw_content_t {}
-
-  /bin/chmod 640 ${dd}/settings.php
-  /bin/chcon -t httpd_sys_content_t ${dd}/settings.php
+  /bin/chmod 640 ${line}/settings.php
+  /bin/chcon -t httpd_sys_content_t ${line}/settings.php
 done
 
 ### for the .htaccess files
@@ -65,13 +63,13 @@ find ${druDir}/ -name '.htaccess' -type f -print0 | xargs -0 -I {} chcon -t http
 
 ### for the .svn and .git directories.
 for d in ${srcCtl}; do
-  dd=${druDir}/${d}/
+  dd=${druDir}/${d}
 
   if [[ -d ${dd} ]]; then
-    find ${dd} -print0 | xargs -0 -I {} chown dev:dev {}
-    find ${dd} -type f -print0 | xargs -0 -I {} chmod 600 {}
-    find ${dd} -type d -print0 | xargs -0 -I {} chmod 0000 {}
-    find ${dd} -type d -print0 | xargs -0 -I {} chmod 700 {}
+    find ${dd}/ -print0 | xargs -0 -I {} chown dev:dev {}
+    find ${dd}/ -type f -print0 | xargs -0 -I {} chmod 600 {}
+    find ${dd}/ -type d -print0 | xargs -0 -I {} chmod 0000 {}
+    find ${dd}/ -type d -print0 | xargs -0 -I {} chmod 700 {}
     
     ### we should add the chon command here
     ### Disabled: because when I "svn update", the SELinux security context permission would become "user_home_t" instead of "httpd_sys_content_t".
