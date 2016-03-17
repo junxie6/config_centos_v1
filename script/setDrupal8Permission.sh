871918c24c7376c498c76904ba092cede37c9c47
Jun Hsieh <jun.github@ijun.org>
2016-03-17 19:04:02 +0000

    up

--- script/setDrupal8Permission.sh
+++ script/setDrupal8Permission.sh
@@ -46,25 +46,28 @@ find ${druDir}/ -not -path "*/\.svn" -and -not -path "*/\.git" -print0 | xargs -
 find ${druDir}/ -not -path "*/\.svn" -and -not -path "*/\.git" -type d -print0 | xargs -0 -I {} chmod 2750 {}
 find ${druDir}/ -not -path "*/\.svn" -and -not -path "*/\.git" -type f -print0 | xargs -0 -I {} chmod 640 {}
 
-### sites
+### sites/*/files and settings.php
 find ${druDir}/sites/ -mindepth 1 -maxdepth 1 -type d -not -path ${druDir}/sites/default | while read line; do
   if [[ -d ${line} ]]; then
+    ### files
     find ${line}/files/ -print0 | xargs -0 -I {} chown php-fpm:web {}
     find ${line}/files/ -print0 | xargs -0 -I {} chcon -t httpd_sys_rw_content_t {}
 
     find ${line}/files/ -type d -print0 | xargs -0 -I {} chmod 2750 {}
     find ${line}/files/ -type f -print0 | xargs -0 -I {} chmod 640 {}
 
-    /bin/chmod 640 ${line}/settings.php
+    ### settings.php
+    /bin/chown dev:web ${line}/settings.php
     /bin/chcon -t httpd_sys_content_t ${line}/settings.php
+    /bin/chmod 440 ${line}/settings.php
   fi
 done
 
-### for the .htaccess files
-find ${druDir}/ -name '.htaccess' -maxdepth 2 -type f -print0 | xargs -0 -I {} chown dev:dev {}
-find ${druDir}/ -name '.htaccess' -maxdepth 2 -type f -print0 | xargs -0 -I {} chcon -t httpd_sys_content_t {}
-
-find ${druDir}/ -name '.htaccess' -maxdepth 2 -type f -print0 | xargs -0 -I {} chmod 644 {}
+### for the .htaccess files within maxdepth 2
+find ${druDir}/ -maxdepth 2 -type f -name '.htaccess' -print0 | xargs -0 -I {} chown dev:dev {}
+find ${druDir}/ -maxdepth 2 -type f -name '.htaccess' -print0 | xargs -0 -I {} chcon -t httpd_sys_content_t {}
+                                                     
+find ${druDir}/ -maxdepth 2 -type f -name '.htaccess' -print0 | xargs -0 -I {} chmod 644 {}
 
 ### for the .svn and .git directories.
 for d in ${srcCtl}; do
