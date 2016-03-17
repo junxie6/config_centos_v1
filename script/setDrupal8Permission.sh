ac622d94909040b0df54a243bfe5fbbb431c9923
Jun Hsieh <jun.github@ijun.org>
2016-03-17 18:38:57 +0000

    up

--- script/setDrupal8Permission.sh
+++ script/setDrupal8Permission.sh
@@ -59,7 +59,7 @@ find ${druDir}/sites/ -mindepth 1 -maxdepth 1 -type d -not -path ${druDir}/sites
 done
 
 ### for the .htaccess files
-find ${druDir}/ -name '.htaccess' -type f -print0 | xargs -0 -I {} chown dev:dev {}
+find ${druDir}/ -name '.htaccess' -type f -print0 | xargs -0 -I {} chown php-fpm:dev {}
 find ${druDir}/ -name '.htaccess' -type f -print0 | xargs -0 -I {} chmod 644 {}
 find ${druDir}/ -name '.htaccess' -type f -print0 | xargs -0 -I {} chcon -t httpd_sys_content_t {}
 
@@ -68,14 +68,10 @@ for d in ${srcCtl}; do
   dd=${druDir}/${d}
 
   if [[ -d ${dd} ]]; then
-    find ${dd}/ -print0 | xargs -0 -I {} chown dev:dev {}
+    find ${dd}/ -print0 | xargs -0 -I {} chown dev:web {}
     find ${dd}/ -type f -print0 | xargs -0 -I {} chmod 600 {}
-    find ${dd}/ -type d -print0 | xargs -0 -I {} chmod -s {}
-    find ${dd}/ -type d -print0 | xargs -0 -I {} chmod 700 {}
-    
-    ### we should add the chon command here
-    ### Disabled: because when I "svn update", the SELinux security context permission would become "user_home_t" instead of "httpd_sys_content_t".
-    #chcon -R -t user_home_t ${dd}
+    find ${dd}/ -type d -print0 | xargs -0 -I {} chmod 2700 {}
+    find ${dd}/ -print0 | xargs -0 -I {} chcon -t httpd_sys_content_t {}
   fi
 done
 
